8b023326069a4f207004be654d0b234472e8f5e5
Jaarknstein <jaarknstein@users.noreply.github.com>
2015-09-25 13:45:18 +1000

    Developed some of the suite
    Testing with suites

--- ITC515_NewLibrary/src/library/test/TestIntegrationSuite.java
+++ ITC515_NewLibrary/src/library/test/TestIntegrationSuite.java
@@ -1,5 +1,20 @@
 package library.test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	//Entity
+	
+	//DAO
+	
+	//CTL
+	
+	//Scenario
+})
+
 public class TestIntegrationSuite {
+
 
 }
