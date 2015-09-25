ea2b6c0a2d6d59c7986ddf9c44231ceadbfe8f06
Jaarknstein <jaarknstein@users.noreply.github.com>
2015-09-25 13:47:34 +1000

    Devleloped some of the TestAll Suite
    Testing with the tests

--- ITC515_NewLibrary/src/library/test/TestAll.java
+++ ITC515_NewLibrary/src/library/test/TestAll.java
@@ -1,5 +1,17 @@
 package library.test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	//Unit
+	
+	//Intergration
+	
+	
+})
+
 public class TestAll {
 
 }
